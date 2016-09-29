eb30cfbfb5f0ab3d879b356f3c4e7106e90c0fb9
Lucas Brito <lucas.brito@embedded.ufcg.edu.br>
2016-09-29 11:34:44 -0300

    added default font

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#000F14" :foreground "#fff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "PfEd" :family "Source Code Pro for Powerline")))))
 
 (load-theme 'deep-thought)
 (set-cursor-color "#ffffff")
