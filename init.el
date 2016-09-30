61d9fda386a18a456dd03c8f8124caa743d70c1f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2016-09-29 21:32:31 -0300

    Added config to org file
    Organized the configuration in literate programming style

--- init.el
+++ init.el
@@ -1,47 +1,9 @@
-;;
-;; INIT.EL FILE
-;;
 
-;; melpa
-(require 'package)
-
-(add-to-list 'package-archives
-			 '("melpa" . "https://melpa.org/packages/"))
-
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
-
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
 
-;; hide annoying gui modes
-(setq inhibit-startup-screen t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq default-tab-width 4)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-	("7bf64a1839bf4dbc61395bd034c21204f652185d17084761a648251041b70233" default)))
- '(package-selected-packages
-   (quote
-	(spacemacs-theme org-beautify-theme js2-mode flymake-jshint smartparens apache-mode go-mode org-bullets magit web-mode php-mode org linum-relative emmet-mode))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000F14" :foreground "#fff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "PfEd" :family "Source Code Pro for Powerline")))))
-
-(load-theme 'deep-thought)
-(set-cursor-color "#ffffff")
-
-(load "~/.emacs.d/org.el")
-(load "~/.emacs.d/web.el")
+(require 'org)
+(org-babel-load-file "~/.emacs.d/config.org")
--- org.el
+++
@@ -1,7 +0,0 @@
-;; Org mode configurations
-
-(require 'org-bullets)
-
-(add-hook 'org-mode-hook
-		  (lambda()
-			(org-bullets-mode 1)))
