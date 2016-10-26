e1d3a06ddeea39a56ba217d0b6459a93158e7072
Lucas Cordeiro <ecdb.lucas@gmail.com>
2016-10-26 15:47:08 -0300

    added global smartparens and fixed some stuff

--- init.el
+++ init.el
@@ -1,4 +1,9 @@
 (package-initialize)
 
+(custom-set-variables
+ '(package-selected-packages
+   (quote
+	(json-mode web-mode typescript-mode smartparens php-mode org-bullets org-beautify-theme org magit linum-relative jade-mode gruvbox-theme go-mode flymake-jshint evil emmet-mode all-the-icons ac-js2))))
+
 (require 'org)
 (org-babel-load-file "~/.emacs.d/config.org")
