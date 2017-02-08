9074f2242750dfb680a89aa32121b33097e047b9
Lucas Cordeiro <ecdb.lucas@gmail.com>
2017-02-08 20:10:50 -0300

    added flow and updated font

--- init.el
+++ init.el
@@ -1,9 +1,19 @@
 (package-initialize)
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-	(json-mode web-mode typescript-mode smartparens php-mode org-bullets org-beautify-theme org magit linum-relative jade-mode gruvbox-theme go-mode flymake-jshint evil emmet-mode all-the-icons ac-js2))))
+	(js2-mode web-mode-edit-element json-mode web-mode typescript-mode smartparens php-mode org-bullets org-beautify-theme org magit linum-relative jade-mode gruvbox-theme go-mode flymake-jshint evil emmet-mode all-the-icons ac-js2))))
 
 (require 'org)
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
